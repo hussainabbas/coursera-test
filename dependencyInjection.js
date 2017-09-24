1f7320e9a4818cb7a75b3f3134f9dc26ab1837ea
hussainAbbas <hussainabbas365@gmail.com>
2017-09-24 06:50:58 -0700

    new Files

---
+++ dependencyInjection.js
@@ -0,0 +1,22 @@
+(function () {
+  'use strict';
+
+  angular.module('DIApp',[])
+  .controller('DIController',DIController);
+
+  function DIController($scope,$filter,$injector) {
+    $scope.name = "Hussain";
+
+    $scope.upper = function(){
+      var upCase = $filter('uppercase')
+      $scope.name = upCase($scope.name)
+    };
+    console.log($injector.annotate(DIController));
+  }
+
+  function AnnonateMe(name,job,blah){
+    return "blah";
+  }
+  console.log(AnnonateMe);
+
+})();
